2666aabfd249dd977cbc04113ba6223dc154c422
Michał Kołsut <michalkolsut@Air-Micha.home>
2023-12-29 11:54:43 +0100

    Add basic configuration and simple test

---
+++ tests/product_page_add_item.spec.js
@@ -0,0 +1,21 @@
+import {test, expect} from "playwright/test"
+
+
+test("Product Page Add To Basket", async ({page}) => {
+    await page.goto('/');
+    const addToBasketButton =  page.locator('[data-qa="product-button"]').first();
+    const basketCounter = page.locator('[data-qa="header-basket-count"]');
+    
+    await expect(addToBasketButton).toHaveText("Add to Basket");
+    await expect(basketCounter).toHaveText("0");
+
+    await addToBasketButton.click();
+
+    await expect(addToBasketButton).toHaveText("Remove from Basket");
+    await expect(basketCounter).toHaveText("1");
+
+    const checkoutLink = page.getByRole('link', { name: 'Checkout' });
+    await checkoutLink.waitFor();
+    await checkoutLink.click();
+    await page.waitForURL('/basket');
+});
