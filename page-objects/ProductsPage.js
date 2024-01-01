b2bcd22b9169019238486ff908ea8d1ef22ea349
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-01-01 16:18:32 +0100

    Add sorting method

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -5,7 +5,10 @@ import { Navigation } from "./Navigation";
 export class ProductsPage {
   constructor(page) {
     this.page = page;
+
     this.addButton = this.page.locator('[data-qa="product-button"]');
+    this.sortDropDown = this.page.locator('[data-qa="sort-dropdown"]');
+    this.productTitle = this.page.locator('[data-qa="product-title"]');
   }
   visit = async () => {
     await this.page.goto("/");
@@ -23,5 +26,14 @@ export class ProductsPage {
     const basketCounterAfterAdding = await navigation.getBasketCount();
     await expect(specificAddButton).toHaveText("Remove from Basket");
     expect(basketCounterAfterAdding).toBeGreaterThan(basketCounterBeforeAdding);
+  };
+
+  sortByCheapest = async () => {
+    await this.sortDropDown.waitFor();
+    await this.productTitle.first().waitFor();
+    const productTitleBeforeSorting = await this.productTitle.allInnerTexts();
+    await this.sortDropDown.selectOption("price-asc");
+    const productTitleAfterSorting = await this.productTitle.allInnerTexts();
+    expect(productTitleBeforeSorting).not.toEqual(productTitleAfterSorting);
   };
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -7,9 +7,10 @@ test.only("New user full endt-end test journey", async ({ page }) => {
   const productsPage = new ProductsPage(page);
   const navigation = new Navigation(page);
   await productsPage.visit();
+  await productsPage.sortByCheapest();
+  await productsPage.addProductToBasket(0);
   await productsPage.addProductToBasket(1);
   await productsPage.addProductToBasket(2);
-  await productsPage.addProductToBasket(4);
   await navigation.goToCheckout();
 
   const checkout = new Checkout(page);
